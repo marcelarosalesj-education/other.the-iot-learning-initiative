11d83e62eb90b6a2fb5734ed470461f12d517ece
Marcela Rosales <marcelarosalesj.gmail.com>
2016-04-22 01:44:54 +0000

    marcelarosalesj: added main.py

---
+++ InternetOfThings101/main.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/python
+
+import signal
+import sys
+import time
+
+def interruptHandler(signal, frame):
+     sys.exit()
+
+
+if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, interruptHandler)
+
+    while True:
+        print "Hello Internet of Things 101"
+        time.sleep(5)
+
+# End of File
